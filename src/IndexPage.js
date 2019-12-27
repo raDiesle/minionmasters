3c1f9f67327ffe1e5701ebcc52fa694a21652fe9
raDiesle <david.amend@it-amend.de>
2019-12-27 10:38:22 +0100

    more code

---
+++ src/IndexPage.js
@@ -0,0 +1,58 @@
+function IndexPage() {
+
+async function fetchCards() {
+  const response = await fetch("/jobCardProps.json");
+  const data = response.json();
+  return data;
+}
+
+let [cards, setCards] = useState([]);
+  useEffect(() => {
+    fetchCards().then(data => {
+      setCards(data);
+    });
+  }, []);
+
+  const [filterName, setFilterName] = useState("");
+
+  useEffect(()=> {
+    
+  }, [filterName]);
+
+
+  const onFilterByName = value => {
+    setFilterName(value);
+    setCards((cCards) => cCards.filter(card => card.name.startsWith(value)));
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={filterName}
+        onChange={event => onFilterByName(event.target.value)}
+      />
+      <Flipper flipKey={filterName}>
+        <ul style={{ listStyleType: "none", display: "flex", flexWrap: "wrap" }}>
+          {cards.map(({pageId, image}) => (
+            
+            <Flipped key={pageId} flipId={pageId}>
+              <li style={{ width: "100px" }}>
+                <img
+                  style={{ width: "100%" }}
+                  src={
+                    "/img_sharp/" +
+                    image
+                  }
+                  alt={image}
+                />
+              </li>
+            </Flipped>
+          ))}
+        </ul>
+      </Flipper>
+    </div>
+  );
+}
+
+export default IndexPage;
