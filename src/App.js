56dc25c437bfd52a8bfc6ac322a2a91143619fd4
David Amend <amend@crxmarkets.com>
2019-12-28 01:20:58 +0100

    cleanup for simplicity and example

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
-import {Router} from "react-router"
-import {Link, Route} from "react-router-dom"
-import {createBrowserHistory} from "history"
+
 import styled from "styled-components"
-import IndexPage from "./IndexPage"
 import {Contents} from "./BaseComponents"
 
 import "./App.css";
-import {Flipper} from "react-flip-toolkit";
+
+import Cards from "./Cards";
 
 const Header = styled.header`
   padding: 0.75rem 1rem;
@@ -35,59 +33,22 @@ const FlexContents = styled(Contents)`
   justify-content: space-between;
 `;
 
-const StyledLink = styled.a`
-  text-decoration: underline;
-`;
-
-const history = createBrowserHistory();
-const cachedPush = history.push;
-
-// override history.push method to allow to exit animations and delayed FLIP
-history.push = args => {
-    if (typeof args === "string") {
-        return cachedPush(args)
-    }
-    if (args && args.state && args.state.animate) {
-        args.state.animate().then(() => {
-            delete args.state.animate
-            cachedPush(args)
-        })
-    } else {
-        cachedPush(args)
-    }
-}
-
 
 const App = () => (
-    <Router history={history}>
-        <Route
-            render={({location, search}) => {
-                return (
-                    <Flipper
-                        flipKey={`${location.pathname}-${location.search}`}
-                        decisionData={{
-                            location,
-                            search
-                        }}
-                    >
-                        <Header>
-                            <FlexContents>
-                                <div>
-                                    <Link to="/">
-                                        <div
-                                            style={{width: "20px", marginRight: ".5rem"}}
-                                        />
-                                        <h1> MinionMasters</h1>
-                                    </Link>
-                                </div>
-                            </FlexContents>
-                        </Header>
-                        <IndexPage/>
-                    </Flipper>
-                )
-            }}
-        />
-    </Router>
+    <div>
+        <Header>
+            <FlexContents>
+                <div>
+                    <div
+                        style={{width: "20px", marginRight: ".5rem"}}
+                    />
+                    <h1> MinionMasters</h1>
+                </div>
+            </FlexContents>
+        </Header>
+        <Cards/>
+    </div>
+
 );
 
 
--- src/Cards.js
+++ src/Cards.js
@@ -1,5 +1,27 @@
-import {useState} from "react";
+import React, {useEffect, useState} from "react";
+import styled from "styled-components";
+import {Card} from "./Card";
 
-export default function Cards(){
+async function fetchCards() {
+    const response = await fetch("/jobCardProps.json");
+    const data = response.json();
+    return data;
+}
 
+const CardStyle = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+`;
+
+export default function Cards() {
+    let [cards, setCards] = useState([]);
+    useEffect(() => {
+        fetchCards().then(data => {
+            setCards(data);
+        });
+    }, []);
+
+    return <CardStyle>
+        {cards.map(card => <Card card={card}/>)}
+    </CardStyle>
 }
