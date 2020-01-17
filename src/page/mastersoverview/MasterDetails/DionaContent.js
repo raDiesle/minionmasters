bf07ac8c5904dcbd000dec78bf6886cb07a15e42
David Amend <amend@crxmarkets.com>
2020-01-17 20:11:25 +0100

    king puff & milloween

--- src/page/mastersoverview/MasterDetails/DionaContent.js
+++ src/page/mastersoverview/MasterDetails/DionaContent.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import {
+    AbilityUlStyle,
     MasterAbilityDescriptionStyle,
     MasterAbilityHeaderStyle,
+    MasterAbilityImageRoundedStyle,
     MasterAbilityImageStyle,
+    MasterAbilitySubHeader,
+    MasterAbilityUnlocksHeaderStyle,
     MasterHeaderStyle
 } from "../MasterModal";
 import {Tab, TabList, TabPanel, Tabs} from "react-tabs";
 import IconDamage from "./icon-damage.png";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faUnlock} from "@fortawesome/free-solid-svg-icons/faUnlock";
 
 export default function DionaContent() {
     return <div>
@@ -26,55 +32,135 @@ export default function DionaContent() {
                 <Tab>
                     <MasterAbilityImageStyle src={IconDamage} alt="basic attack"/>
                 </Tab>
+                <Tab>
+                    <MasterAbilityImageRoundedStyle src="generated/img/Diona_Perk1.jpg" alt="Diona Perk1"/>
+                </Tab>
+                <Tab>
+                    <MasterAbilityImageRoundedStyle src="generated/img/Diona_Perk2.jpg" alt="Diona Perk2"/>
+                </Tab>
+                <Tab>
+                    <MasterAbilityImageRoundedStyle src="generated/img/Diona_Perk3.jpg" alt="Diona Perk3"/>
+                </Tab>
             </TabList>
             <TabPanel>
-                Basic Attack
-                Rapidly fires bolts from her dual crossbows for 20 damage every 1 sec with a range of 10.
-                Lays a Bear Trap every 20 sec.
-            </TabPanel>
-            <TabPanel>
+                <AbilityUlStyle>
+                    <li>
+                        20 damage every 1 sec
+                    </li>
+                    <li>
+                        range 10
+                    </li>
+                    <li>
+                        Lays a Bear Trap every 20 sec.
+                    </li>
+                </AbilityUlStyle>
+
                 <MasterAbilityHeaderStyle>
                     Bear Trap
                 </MasterAbilityHeaderStyle>
+                <AbilityUlStyle>
+                    <li>
+                        Roots a unit in place for 4 seconds
+                    </li>
+                    <li>
+                        Deals 50 Damage
+                    </li>
+                    <li>
+                        Lasts 15 sec.
+                    </li>
+                </AbilityUlStyle>
+            </TabPanel>
+            <TabPanel>
+                <MasterAbilityHeaderStyle>
+                    Art of the Hunt
+                </MasterAbilityHeaderStyle>
                 <MasterAbilityDescriptionStyle>
-                    Roots a unit in place for 4 seconds
-                    Deals 50 Damage
-                    Lasts 15 sec.
+                    Add a Trap card to Diona's deck.
                 </MasterAbilityDescriptionStyle>
+                <MasterAbilityDescriptionStyle>
+                    The card alternates between Crossbow Trap and Decoy Trap with each draw.
+                </MasterAbilityDescriptionStyle>
+
+                <MasterAbilitySubHeader>
+                    Crossbow Trap
+                </MasterAbilitySubHeader>
+                <AbilityUlStyle>
+                    <li>
+                        Stealth. Fires bolts at nearby minions.
+                    </li>
+                    <li>
+                        Damage per sec: 30
+                    </li>
+                    <li>
+                        Health: 100
+                    </li>
+                    <li>
+                        Lasts 15 sec
+                    </li>
+                    <li>
+                        When used, this card becomes a Decoy Trap
+                    </li>
+                </AbilityUlStyle>
+                <MasterAbilitySubHeader>
+                    Decoy Trap
+                </MasterAbilitySubHeader>
+                <AbilityUlStyle>
+                    <li>
+                        Taunts nearby minions
+                    </li>
+                    <li>
+                        Health: 300
+                    </li>
+                    <li>
+                        Lasts 15 sec
+                    </li>
+                    <li>
+                        When used, this card becomes a Crossbow Trap
+                    </li>
+                </AbilityUlStyle>
+
+                <MasterAbilityUnlocksHeaderStyle>
+                    <FontAwesomeIcon icon={faUnlock}/> 20XP
+                </MasterAbilityUnlocksHeaderStyle>
+            </TabPanel>
+            <TabPanel>
+                <MasterAbilityHeaderStyle>
+                    Fetch!
+                </MasterAbilityHeaderStyle>
+
+                <MasterAbilityDescriptionStyle>
+                    Every 10 sec, Ruffles will run to a killed enemy Minion and dig up a prize.
+                </MasterAbilityDescriptionStyle>
+                Prizes:
+                <AbilityUlStyle>
+                    <li>
+                        2 Mana
+                    </li>
+                    <li>
+                        3 XP
+                    </li>
+                    <li>
+                        250 Master Health
+                    </li>
+                    <MasterAbilityUnlocksHeaderStyle>
+                        <FontAwesomeIcon icon={faUnlock}/> 60XP
+                    </MasterAbilityUnlocksHeaderStyle>
+                </AbilityUlStyle>
+
+            </TabPanel>
+            <TabPanel>
+                <MasterAbilityHeaderStyle>
+                    Thrill of the Hunt
+                </MasterAbilityHeaderStyle>
+                <MasterAbilityDescriptionStyle>
+                    Add another Trap card to Diona's deck.
+                </MasterAbilityDescriptionStyle>
+                <MasterAbilityUnlocksHeaderStyle>
+                    <FontAwesomeIcon icon={faUnlock}/> 120XP
+                </MasterAbilityUnlocksHeaderStyle>
             </TabPanel>
         </Tabs>
 
-        Art of the Hunt
-        Add a Trap card to Diona's deck. The card alternates between Crossbow Trap and Decoy Trap with each draw.
 
-        Crossbow Trap
-        Stealth. Fires bolts at nearby minions.
-        Damage per sec: 30
-        Health: 100
-        Lasts 15 sec
-        When used, this card becomes a Decoy Trap
-
-        Decoy Trap
-        Taunts nearby minions
-        Health: 300
-        Lasts 15 sec
-        When used, this card becomes a Crossbow Trap
-
-
-        Fetch!
-
-        Every 10 sec, Ruffles will run to a killed enemy Minion and dig up a prize.
-
-        Prizes:
-        - 2 Mana
-        -3 XP
-        - 250 Master Health
-
-        (Unlocks at 60XP)
-
-
-        Thrill of the Hunt
-        Add another Trap card to Diona's deck.
-        (Unlocks at 120 XP)
     </div>
 }
--- src/page/mastersoverview/MasterDetails/KingPuffContent.js
+++ src/page/mastersoverview/MasterDetails/KingPuffContent.js
@@ -1,31 +1,97 @@
 import React from "react";
+import {
+    AbilityUlStyle,
+    MasterAbilityDescriptionStyle,
+    MasterAbilityHeaderStyle,
+    MasterAbilityImageRoundedStyle,
+    MasterAbilityImageStyle,
+    MasterAbilityUnlocksHeaderStyle,
+    MasterHeaderStyle
+} from "../MasterModal";
+import {Tab, TabList, TabPanel, Tabs} from "react-tabs";
+import IconDamage from "./icon-damage.png";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faUnlock} from "@fortawesome/free-solid-svg-icons/faUnlock";
 
 export default function KingPuffContent() {
     return <div>
-        King Puff is a trickster, he loves to confuse his opponents and keep bridge control just as he loves kingdom
-        control.
+        <MasterHeaderStyle>Diona</MasterHeaderStyle>
+        <MasterAbilityDescriptionStyle>
+            King Puff is a trickster, he loves to confuse his opponents and keep bridge control just as he loves kingdom
+            control.
+        </MasterAbilityDescriptionStyle>
 
-        Basic Attac
-        A slow but powerful attack dealing 75 damage to a single unit every 2.5 sec, with a range of 8.
+        <Tabs>
+            <TabList>
+                <Tab>
+                    <MasterAbilityImageStyle src={IconDamage} alt="basic attack"/>
+                </Tab>
+                <Tab>
+                    <MasterAbilityImageRoundedStyle src="generated/img/KingPuff_TrickSwap.png" alt="KingPuff Perk1"/>
+                </Tab>
+                <Tab>
+                    <MasterAbilityImageRoundedStyle src="generated/img/KingPuff_Inspire.jpg" alt="KingPuff Perk2"/>
+                </Tab>
+                <Tab>
+                    <MasterAbilityImageRoundedStyle src="generated/img/KingPuff_BridgeShield.png" alt="KingPuff Perk3"/>
+                </Tab>
+            </TabList>
+            <TabPanel>
+                <MasterAbilityHeaderStyle>
+                    Basic Attack
+                </MasterAbilityHeaderStyle>
+                A slow but powerful attack dealing 75 damage to a single unit every 2.5 sec, with a range of 8.
+            </TabPanel>
+            <TabPanel>
+                <MasterAbilityHeaderStyle>
+                    Trick Swap
+                </MasterAbilityHeaderStyle>
 
-        Trick Swap
-        Add the card Trick Swap to King Puff's deck.
+                <MasterAbilityDescriptionStyle>
+                    Add the card Trick Swap to King Puff's deck.
+                </MasterAbilityDescriptionStyle>
+                <MasterAbilityDescriptionStyle>
+                    Swaps the location of ALL minions on both bridges and Stuns enemies for 4 sec.
+                </MasterAbilityDescriptionStyle>
 
-        Swaps the location of ALL minions on both bridges and Stuns enemies for 4 sec.
+                <MasterAbilityUnlocksHeaderStyle>
+                    <FontAwesomeIcon icon={faUnlock}/> 20XP
+                </MasterAbilityUnlocksHeaderStyle>
+            </TabPanel>
 
-        King Buff
-        King Puff's 4 and 5 mana Minion Cards gain either Rage or Shield when played.
+            <TabPanel>
+                <MasterAbilityHeaderStyle>
+                    King Buff
+                </MasterAbilityHeaderStyle>
 
-        Rage: +50% Attack damage.
+                King Puff's 4 and 5 mana Minion Cards gain either Rage or Shield when played.
+                <AbilityUlStyle>
+                    <li>
+                        Rage: +50% Attack damage.
+                    </li>
+                    <li>
+                        Shield: Blocks all damage from the next damage source.
+                    </li>
+                </AbilityUlStyle>
+                <MasterAbilityUnlocksHeaderStyle>
+                    <FontAwesomeIcon icon={faUnlock}/> 60XP
+                </MasterAbilityUnlocksHeaderStyle>
+            </TabPanel>
 
-        Shield: Blocks all damage from the next damage source.
-
-        (Unlocks at 60 XP)
-
-        Bridge Shield
-        King Puff marks the bottom bridge and is immune to damage when controlling the marked bridge.
-
-        Trick Swap moves the mark.
-        (Unlocks at 12XP)
+            <TabPanel>
+                <MasterAbilityHeaderStyle>
+                    Bridge Shield
+                </MasterAbilityHeaderStyle>
+                <MasterAbilityDescriptionStyle>
+                    King Puff marks the bottom bridge and is immune to damage when controlling the marked bridge.
+                </MasterAbilityDescriptionStyle>
+                <MasterAbilityDescriptionStyle>
+                    Trick Swap moves the mark.
+                </MasterAbilityDescriptionStyle>
+                <MasterAbilityUnlocksHeaderStyle>
+                    <FontAwesomeIcon icon={faUnlock}/> 120XP
+                </MasterAbilityUnlocksHeaderStyle>
+            </TabPanel>
+        </Tabs>
     </div>
 }
