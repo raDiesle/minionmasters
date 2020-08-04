d21fb5ea8e051eed801792421e7a9664d14726d1
David Amend <amend@crxmarkets.com>
2020-08-04 18:22:05 +0200

    fixes by Steven, fix to take game data for type and attack, add achievements

--- src/attack/targetsMapping.js
+++ src/attack/targetsMapping.js
@@ -1,3 +1,5 @@
+import { faMagic } from "@fortawesome/free-solid-svg-icons/faMagic";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import BuildingTargetIcon from "attack/building-target-icon";
 import GroundIcon from "./GroudIcon";
@@ -7,5 +9,6 @@ export const targetsMapping = {
   Ground: <GroundIcon />,
   "Ground & Air": <GroundAirTargetIcon />,
   Building: <BuildingTargetIcon />,
+  "Is Spell": <FontAwesomeIcon icon={faMagic} />,
   // "Air":  <GroundAndAirIcon/> switched icons
 };
