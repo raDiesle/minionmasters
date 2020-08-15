dc3312ad2244c5349a45e497ba44ec91a8aeb799
David Amend <amend@crxmarkets.com>
2020-08-15 15:28:13 +0200

    fix two wildcards to be visible and have them render above

--- src/page/carddeck/card-deck-slot.js
+++ src/page/carddeck/card-deck-slot.js
@@ -1,37 +1,9 @@
 import { IDENTIFIER_FOR_EMPTY_SLOT } from "page/carddeck/deck-manager";
 import React from "react";
-import styled from "styled-components";
 import CardDeckSlotStyle from "../../CardDeckSlotStyle";
 import EmptyCardSlotSelected from "../../EmptyCardSlotSelected";
 import { Card } from "../Card";
-
-const EmptyCardSlotUnselectedStyle = styled.div`
-  width: 100%;
-  height: calc(100% + 2px);
-  margin-top: 1px;
-  border: 1px solid #fff;
-`;
-
-const WildcardOverlayStyle = styled.div`
-  position: relative;
-
-  color: rgb(240, 255, 51);
-  font-weight: bolder;
-
-  top: -86px;
-  right: -8px;
-  font-size: 14px;
-
-  @media (max-width: 767px) {
-    top: -54px;
-    right: -5px;
-    font-size: 20px;
-  }
-
-  &:hover {
-    cursor: pointer;
-  }
-`;
+import css from "./card-deck-slot.module.scss";
 
 export function CardDeckSlot({
   lastSelectedCard: {
@@ -46,21 +18,30 @@ export function CardDeckSlot({
     <CardDeckSlotStyle isSelectedSlot={isSelectedSlot}>
       {iD !== IDENTIFIER_FOR_EMPTY_SLOT ? (
         <>
-          <Card card={card} isDeckCard showDeck isFullWidthClickable>
-            {cardActionWrapper(card)}
-          </Card>
+          <div className={css.oneCardStyle}>
+            <Card card={card} isDeckCard showDeck isFullWidthClickable>
+              {cardActionWrapper(card)}
+            </Card>
+          </div>
           {count > 1 && (
-            <WildcardOverlayStyle>
+            <div className={css.wildcardOverlaySecondStyle}>
               <Card card={card} isDeckCard showDeck isFullWidthClickable>
                 {cardActionWrapper(card)}
               </Card>
-            </WildcardOverlayStyle>
+            </div>
+          )}
+          {count > 2 && (
+            <div className={css.wildcardOverlayThirdStyle}>
+              <Card card={card} isDeckCard showDeck isFullWidthClickable>
+                {cardActionWrapper(card)}
+              </Card>
+            </div>
           )}
         </>
       ) : isSelectedSlot ? (
         <EmptyCardSlotSelected>Select Card</EmptyCardSlotSelected>
       ) : (
-        <EmptyCardSlotUnselectedStyle>&nbsp;</EmptyCardSlotUnselectedStyle>
+        <div className={css.EmptyCardSlotUnselectedStyle}>&nbsp;</div>
       )}
     </CardDeckSlotStyle>
   );
--- src/page/carddeck/deck-manager.js
+++ src/page/carddeck/deck-manager.js
@@ -156,7 +156,6 @@ export default function DeckManager() {
               <CardForDeckActionOverlay card={card} setSelectedCardEvent={setSelectedCardEvent} />
             )}
           />
-          ;
         </TabPanel>
         <TabPanel>
           <SaveDeckContainer lastSelectedCards={lastSelectedCards} selectedHero={selectedHero} />
