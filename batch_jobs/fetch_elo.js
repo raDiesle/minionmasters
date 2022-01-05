e0788b4ef580173725edf268582d4b0b84c1efc9
David Amend <amend@crxmarkets.com>
2022-01-05 21:33:08 +0100

    elo

--- batch_jobs/fetch_elo.js
+++ batch_jobs/fetch_elo.js
@@ -1,39 +1,13 @@
+const orderBy = require("lodash/orderBy");
+
 const fetch = require("node-fetch");
 
 const http = require("http");
 const fs = require("fs");
 
-const download = (url, path) =>
-  new Promise((resolve, reject) => {
-    http.get(url, (response) => {
-      const statusCode = response.statusCode;
-
-      if (statusCode !== 200) {
-        return reject("Download error!");
-      }
-      let body = '';
-      response.on('data', function (chunk) {
-        body = body + chunk;
-      });
-
-      const writeStream = fs.createWriteStream(path);
-      response.pipe(writeStream);
-
-      writeStream.on("error", (err ) => {
-        console.error("Error writing to file!" + err.message);
-        reject("Error writing to file!")
-      });
-      writeStream.on("finish", () => {
-        writeStream.close(() => resolve(JSON.parse(body)));
-      });
-
-      response.on('end', function() {
-        console.log("single download finished");
-      });
-    });
-  }).catch((err) => console.error(err));
 
 const TARGET_FOLDER = "./batch_jobs/elo";
+
 
 
 const totalResults = [];
@@ -43,21 +17,72 @@ let limitStart = 0;
 let count = 0;
 // http://fdmfdm.nl/EloChecker.html
 console.log("start downloading");
-(function loop() {
+(async function loop() {
   if (currentResults === null || currentResults.length !== 0) {
     const url = `http://fdmfdm.nl/GetAllUserElo.php?limitStart=${limitStart}&limitStep=${limitStep}`;
     console.log(url);
     console.log(`Fetching count : ${count} totalResults: ${totalResults.length}`);
-    download(url, TARGET_FOLDER + "/elo_" + count + ".json")
-      .then(data => {
-        debugger;
-      currentResults = data;
-      totalResults.push(...data);
-      limitStart = limitStart + limitStep;
-      count = count + 1;
-      loop();
-    });
+    const response = await fetch(url);
+    const data = await response.json();
+
+    currentResults = data;
+
+    const normalized = currentResults.map(({ User_id, Id, Elo1v1, Elo2v2Team, Elo2v2Solo }) => ({
+      User_id: parseInt(User_id),
+      Id: parseInt(Id),
+      Elo1v1: parseInt(Elo1v1),
+      Elo2v2Team: parseInt(Elo2v2Team),
+      Elo2v2Solo: parseInt(Elo2v2Solo)
+    }));
+
+
+
+    const prevLimited = normalized.filter(({Elo1v1, Elo2v2Team, Elo2v2Solo}) => Elo1v1 > 1600 || Elo2v2Team > 1600 || Elo2v2Solo > 1600 );
+
+    console.log(prevLimited.length)
+    // const limited = sortedBy.slice(0, 50000);
+
+    totalResults.push(...prevLimited);
+    limitStart = limitStart + limitStep;
+    count = count + 1;
+    await loop();
   }
+
+  const sortedBy = orderBy(totalResults, ["Elo2v2Solo"], ["desc"]);
+
+  fs.writeFile(`./batch_jobs/elo/all.json`, JSON.stringify(sortedBy), err => {
+    if (err) {
+      console.error(err)
+      return;
+    }
+  })
+
 }());
+
+
+
 console.log("downloads ended");
 
+const players = [1575050];
+let pos = 0;
+(async function loopSingles() {
+  if (currentResults === null || currentResults.length !== 0) {
+  //  const response = await fetch(url);
+  //  const data = await response.json();
+
+        const response = await fetch(`http://fdmfdm.nl/GetUserElo.php?userID=${players[pos]}`);
+        const playerResponse = await response.json();
+        const singlePlayer = playerResponse[0];
+
+        fs.writeFile(`./batch_jobs/elo/${singlePlayer.User_id}.json`, JSON.stringify(singlePlayer), err => {
+          if (err) {
+            console.error(err)
+            return;
+          }
+        });
+
+        pos = pos+1;
+  }
+}());
+
+console.log("Everything completed");
---
+++ src/page/elo/save-mapping.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { db, dbErrorHandlerPromise } from "mm-firestore";
+import mToast from "components/mToast";
+import * as classnames from "classnames";
+import cssButton from "components/button.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSave } from "@fortawesome/free-solid-svg-icons/faSave";
+
+import css from "./save-mapping.module.scss";
+
+export function SaveMapping({isMappingMode, setIsMappingMode}){
+  const [playerId, setPlayerId] = useState("");
+  const [username, setUsername] = useState("");
+  const handlePlayerId = (e) => {setPlayerId(e.currentTarget.value)};
+  const handleUsername = (e) => {setUsername(e.currentTarget.value)};
+  const handleSave = () => {
+
+    db.collection("playermappings").doc(playerId).set({ username }).then((docRef) => {
+      console.log("Document written with ID: ", docRef.id);
+    })
+    .catch(dbErrorHandlerPromise);
+    setIsMappingMode(false);
+    mToast("Mapping is stored");
+
+  };
+
+  return <div>
+    <div>
+      <h2>Register Mapping</h2>
+      <div>Register a UserId to Username mapping:</div>
+      <ul>
+      <li>Visitors will see username in the list.</li>
+      <li>All registered players, elo will be stored over time to compare in charts.</li>
+      </ul>
+      {!isMappingMode && <button
+        className={classnames(cssButton.ButtonInGroupStyle, cssButton.buttonSpacing)}
+        onClick={() => {
+        setIsMappingMode(true);
+      }}> <FontAwesomeIcon icon={faSave} /> Add a mapping</button>}
+
+      {isMappingMode && <div className={css.form}>
+
+        <input type="text" placeholder="UserId" onChange={(e) => handlePlayerId(e)} />
+
+
+        <input type="text" placeholder="Username" onChange={(e) => handleUsername(e)} />
+
+
+        <button type="button"
+                className={classnames(cssButton.ButtonInGroupStyle, cssButton.buttonSpacing, css.saveButton)}
+                onClick={() => handleSave()}> <FontAwesomeIcon icon={faSave} /> Save</button>
+
+      </div>}
+  </div>
+  </div>;
+}
