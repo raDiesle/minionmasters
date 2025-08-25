6155b1d5203817d3a11397a0770d81d86b9dbdef
Novryk <gunantsdrillydrill@gmail.com>
2025-08-25 16:49:52 +0200

    end of season public stats added

--- src/page/public-stats/public-stats-config.js
+++ src/page/public-stats/public-stats-config.js
@@ -2,5 +2,6 @@ export const ROUTE_PATH_PUBLIC_STATS = "/public-stats";
 export const ROUTE_PATH_CARD_STATS = "/public-stats";
 export const ROUTE_PATH_MASTER_STATS = "/public-stats/masters";
 
+export const EOS_PATH_SUFFIX = "/end-of-season";
 export const API_KEY = 'AIzaSyCDewMN1O6CBrfHbmyJwQ9tmPPCEm6IItQ';   //Google API
 export const SHEET_ID = '1FtctI6HWI6aKIBZQ-uCtWrQwKXEIfXNh55EAU4DWWGQ';
